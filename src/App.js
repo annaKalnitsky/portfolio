4b50483314a546b50e042687bbc4f621f8d1a753
Anna Kalnitsky <annkalnitsky@gmail.com>
2023-03-15 14:35:10 +0200

    routing

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import About from './components/About/About';
+import Home from './components/Home/Home';
+import Portfolio from './components/Portfolio/Portfolio';
+import Contacts from './components/Contacts/Contacts';
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Contacts/Contacts.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.css';
+
+const Contacts = () => {
+  return (
+    <div>
+      <h1>Contacts</h1>
+    </div>
+  );
+};
+
+export default Contacts;
